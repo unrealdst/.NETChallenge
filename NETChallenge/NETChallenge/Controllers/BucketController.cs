435534b49ad4874af3fedd002809fa7b5d4cd92a
Unrealdst unrealdst <unrealdst@gmail.com>
2018-09-25 15:01:12 +0200

    Init commit

---
+++ NETChallenge/NETChallenge/Controllers/BucketController.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using NETChallenge.Models;
+
+namespace NETChallenge.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class BucketController : Controller
+    {
+        private Bucket Bucket = new Bucket();
+
+        [HttpGet]
+        public Bucket Get()
+        {
+            return Bucket;
+        }
+
+        [HttpPost]
+        public void Post([FromBody]Item item)
+        {
+            Bucket.Items.Add(item);
+        }
+
+        [HttpPut("{Id}")]
+        public void Quantity(int id, int Quantity)
+        {
+            if(Quantity == 0)
+            {
+                Bucket.Items.Remove(Bucket.Items.Single(x => x.Id == id));
+                return;
+            }
+            Bucket.Items.Single(x => x.Id == id).Quantity = Quantity;
+        }
+    }
+}
---
+++ NETChallenge/NETChallenge/Models/Bucket.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace NETChallenge.Models
+{
+    public class Bucket
+    {
+        public Bucket()
+        {
+            if(_items == null)
+            {
+                _items = new List<Item>();
+            }
+        }
+
+        private static List<Item> _items;
+
+        public List<Item> Items { get => _items; set => _items = value; }
+    }
+}
